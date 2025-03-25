c1e564da1c6db88dfb7984576f7e46884c30e8fa
Hugi Thordarson <hugi@karlmenn.is>
2025-03-25 15:39:58 +0000

    Nice styling on error overlay div

--- src/main/java/parsley/experimental/ParsleyRequestObserver.java
+++ src/main/java/parsley/experimental/ParsleyRequestObserver.java
@@ -16,8 +16,8 @@ public class ParsleyRequestObserver {
 	public void didHandleRequest( NSNotification notification ) {
 		if( !errors.get().isEmpty() ) {
 			String errorDiv = """
-					<div style="width: 100%%; height: 100px; position: absolute; top: 0px; right: 0px; background-color: rgba(255,0,0,0.6); border-bottom: 2px solid red; color: white; padding: 30px; text-align: center">
-						<h2>%s %s errors on page</h2>
+					<div style="width: 100%%; height: 100px; position: absolute; top: 0px; right: 0px; background-color: rgba(255,0,0,0.6); border-bottom: 2px solid red; color: white; padding: 32px; text-align: center; text-shadow: 1px 1px 2px black">
+						<h2 style="font-size: 24px">%s %s errors on page</h2>
 					</div>
 					""".formatted( ParsleyConstants.HERB, String.valueOf( errors.get().size() ) );
 			//			String errorDiv = """
