4b125f7b79158c69787dfb949fd7a720c0053ffa
Hugi Thordarson <hugi@karlmenn.is>
2025-03-25 15:28:56 +0000

    Keep our precious HERB in a constant

--- src/main/java/parsley/ParsleyErrorMessageElement.java
+++ src/main/java/parsley/ParsleyErrorMessageElement.java
@@ -7,6 +7,8 @@ import com.webobjects.appserver.WOElement;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 
+import parsley.experimental.ParsleyConstants;
+
 public class ParsleyErrorMessageElement extends WOElement {
 
 	private final String _message;
@@ -28,22 +30,15 @@ public class ParsleyErrorMessageElement extends WOElement {
 		// If we have an exception, make it clickable
 		final String elementName = _exception != null ? "a" : "span";
 		final String urlString = _exception != null ? "href=\"%s\"".formatted( context.componentActionURL() ) : "";
-		final String herb = unicodeCharWithName( "HERB" );
 
 		response.appendContentString( """
 				<%1$s %3$s style="display: inline-block; font-size: 16px !important; text-align: left !important; color: white; background-color: rgba(255,0,0,0.8); padding: 10px; margin: 10px">
 					<span style="display: inline-block; width: 28px; vertical-align: top">%4$s</span>
 					<span style="display: inline-block">%2$s</span>
 				</%1$s>
-				""".formatted( elementName, _message, urlString, herb ) );
-	}
+				""".formatted( elementName, _message, urlString, ParsleyConstants.HERB ) );
 
-	/**
-	 * @return The unicode character corresponding to the given character name
-	 */
-	private static String unicodeCharWithName( final String unicodeCharacterName ) {
-		final int codePoint = Character.codePointOf( unicodeCharacterName );
-		return Character.toString( codePoint );
+		Parsley.requestObserver.errors.get().add( _message );
 	}
 
 	@Override
---
+++ src/main/java/parsley/experimental/ParsleyConstants.java
@@ -0,0 +1,14 @@
+package parsley.experimental;
+
+public class ParsleyConstants {
+
+	public static final String HERB = unicodeCharWithName( "HERB" );
+
+	/**
+	 * @return The unicode character corresponding to the given character name
+	 */
+	private static String unicodeCharWithName( final String unicodeCharacterName ) {
+		final int codePoint = Character.codePointOf( unicodeCharacterName );
+		return Character.toString( codePoint );
+	}
+}
