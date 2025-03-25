5f23eda65c7d167c3729ebe4501a3ef9bbde8326
Hugi Thordarson <hugi@karlmenn.is>
2025-03-25 16:18:23 +0000

    Add a second overlay to make an error situation even worse looking

--- src/main/java/parsley/experimental/ParsleyRequestObserver.java
+++ src/main/java/parsley/experimental/ParsleyRequestObserver.java
@@ -21,6 +21,9 @@ public class ParsleyRequestObserver {
 					<div style="width: 100%%; height: 100px; position: fixed; top: 0px; right: 0px; background-color: rgba(255,0,0,0.6); border-bottom: 2px solid red; color: white; padding: 32px; text-align: center; text-shadow: 1px 1px 2px black; pointer-events: none; z-index: 2147483647">
 						<h2 style="font-size: 24px">%s %s %s on page</h2>
 					</div>
+					<div style="width: 100%%; height: 100%%; position: fixed; top: 100px; right: 0px; background-color: rgba(255,140,0,0.1); pointer-events: none; z-index: 2147483646; border: 0px solid red">
+						&nbsp;
+					</div>
 					""".formatted( ParsleyConstants.HERB, errorCount, (errorCount == 1 ? "error" : "errors") );
 			//			String errorDiv = """
 			//					<div style="width: 1000px; height: 400px; position: absolute; top: 0px; right: 0px; background-color: rgba(255,0,0,0.8); border: 2px solid red; color: white; overflow: scroll; padding: 10px">
