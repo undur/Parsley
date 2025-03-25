a6d14179defd4dda94ab38ab918132aa05b26c7c
Hugi Thordarson <hugi@karlmenn.is>
2025-03-25 10:07:24 +0000

    Sync association creation with ng-objects' current association factory

--- src/main/java/parsley/ParsleyAssociationFactory.java
+++ src/main/java/parsley/ParsleyAssociationFactory.java
@@ -10,34 +10,13 @@ import com.webobjects.appserver._private.WOKeyValueAssociation;
 import ng.appserver.templating.parser.NGDeclaration.NGBindingValue;
 import parsley.experimental.ParsleyKeyValueAssociation;
 
-/**
- * FIXME: Everything here needs to be refactored and fixed up // Hugi 2024-11-24
- */
-
 public class ParsleyAssociationFactory {
 
-	public static final WOConstantValueAssociation TRUE = new WOConstantValueAssociation( Boolean.TRUE );
-	public static final WOConstantValueAssociation FALSE = new WOConstantValueAssociation( Boolean.FALSE );
-
-	public static WOAssociation constantValueAssociationWithValue( Object obj ) {
-		return new WOConstantValueAssociation( obj );
-	}
-
-	public static WOAssociation associationWithKeyPath( String keyPath ) {
-
-		if( keyPath.charAt( 0 ) == '^' ) {
-			return new WOBindingNameAssociation( keyPath.substring( 1 ) ); // FIXME: WOOgnl actually uses a different class here with some fixes that might matter // Hugi 2024-11-24
-		}
-
-		if( Parsley.showInlineErrorMessagesForRenderingErrors ) {
-			return new ParsleyKeyValueAssociation( keyPath );
-		}
-
-		return new WOKeyValueAssociation( keyPath );
-	}
+	private static final WOConstantValueAssociation TRUE = new WOConstantValueAssociation( Boolean.TRUE );
+	private static final WOConstantValueAssociation FALSE = new WOConstantValueAssociation( Boolean.FALSE );
 
 	/**
-	 * FIXME: This is kind of shitty // Hugi 2024-11-16
+	 * @return An association for the given binding value
 	 */
 	public static WOAssociation associationForBindingValue( final NGBindingValue bindingValue, final boolean isInline ) {
 
@@ -49,7 +28,7 @@ public class ParsleyAssociationFactory {
 	}
 
 	/**
-	 * FIXME: This is kind of shitty // Hugi 2024-11-16
+	 * @return An association for the given inline binding value
 	 */
 	private static WOAssociation associationForInlineBindingValue( String value ) {
 		Objects.requireNonNull( value );
@@ -70,46 +49,48 @@ public class ParsleyAssociationFactory {
 				if( value.endsWith( "VALID" ) ) {
 					value = value.replaceFirst( "\\s*//\\s*VALID", "" );
 				}
-
-				return associationForWodBindingValue( value, false );
 			}
 			else {
-				// FIXME: Figure out what the absolute ding-diddly we're doing here // Hugi 2024-11-23
-				value = value.replaceAll( "\\\\\\$", "\\$" );
-				value = value.replaceAll( "\\\"", "\"" );
-				return associationForWodBindingValue( value, true );
+				value = value.replace( "\\$", "$" ); // Unescape escaped dollar signs
+				value = value.replace( "\\\"", "\"" ); // Unescape escaped quotes
+				return associationForConstantStringValue( value );
 			}
 		}
 
-		return associationForWodBindingValue( value, false );
+		return associationForDynamicValue( value );
 	}
 
 	/**
-	 * FIXME: This is kind of shitty // Hugi 2024-11-16
+	 * @return An association for the given wod binding value
 	 */
-	private static WOAssociation associationForWodBindingValue( String associationValue, final boolean isQuoted ) {
-		Objects.requireNonNull( associationValue );
+	private static WOAssociation associationForWodBindingValue( final String associationValue, final boolean isQuoted ) {
 
 		if( isQuoted ) {
-			// MS: WO 5.4 converts \n to an actual newline. I don't know if WO 5.3 does, too, but let's go ahead and be compatible with them as long as nobody is yelling.
-			// FIXME: Escaping needs to be thought through and standardized, for both wod and inline bindings // Hugi 2024-11-23
-			associationValue = applyEscapes( associationValue );
-			return constantValueAssociationWithValue( associationValue );
+			return associationForConstantStringValue( associationValue );
 		}
+
+		return associationForDynamicValue( associationValue );
+	}
+
+	/**
+	 * @return An association for the given constant string value, by applying the necessary escapes
+	 */
+	private static WOAssociation associationForConstantStringValue( String associationValue ) {
+		associationValue = applyEscapes( associationValue );
+		return associationForConstantValue( associationValue );
+	}
+
+	/**
+	 * @return And association for the given dynamic value (a "dynamic value" being what we're calling any value following a $ in an inline binding or an unquoted value in a wod binding)
+	 */
+	private static WOAssociation associationForDynamicValue( final String associationValue ) {
 
 		if( isNumeric( associationValue ) ) {
-			final Number number;
-
-			if( associationValue.contains( "." ) ) {
-				number = Double.valueOf( associationValue );
-			}
-			else {
-				number = Integer.parseInt( associationValue );
-			}
-
-			return constantValueAssociationWithValue( number );
+			final Number number = numericValueFromString( associationValue );
+			return associationForConstantValue( number );
 		}
 
+		// FIXME: I really think only $true and $false should be interpreted as boolean values in inline bindings // Hugi 2025-03-19
 		if( "true".equalsIgnoreCase( associationValue ) || "yes".equalsIgnoreCase( associationValue ) ) {
 			return TRUE;
 		}
@@ -118,75 +99,112 @@ public class ParsleyAssociationFactory {
 			return FALSE;
 		}
 
-		return associationWithKeyPath( associationValue );
+		return associationForKeyPath( associationValue );
+	}
+
+	/**
+	 * @return An association that returns the given value
+	 */
+	private static WOAssociation associationForConstantValue( final Object value ) {
+		return new WOConstantValueAssociation( value );
+	}
+
+	/**
+	 * @return An association for resolving the given keyPath
+	 */
+	private static WOAssociation associationForKeyPath( final String keyPath ) {
+
+		if( keyPath.charAt( 0 ) == '^' ) {
+			return new WOBindingNameAssociation( keyPath.substring( 1 ) );
+		}
+
+		if( Parsley.showInlineErrorMessagesForRenderingErrors ) {
+			return new ParsleyKeyValueAssociation( keyPath );
+		}
+
+		return new WOKeyValueAssociation( keyPath );
 	}
 
 	/**
 	 * @return The given string with escape sequences \r, \n and \t converted to what they represent
 	 */
-	private static String applyEscapes( String string ) {
-		int backslashIndex = string.indexOf( '\\' );
+	static String applyEscapes( String string ) {
 
-		if( backslashIndex != -1 ) {
-			StringBuilder sb = new StringBuilder( string );
-			int length = sb.length();
+		int firstBackslashIndex = string.indexOf( '\\' );
 
-			for( int i = backslashIndex; i < length; i++ ) {
-				char ch = sb.charAt( i );
-				if( ch == '\\' && i < length ) {
-					char nextCh = sb.charAt( i + 1 );
-					if( nextCh == 'n' ) {
-						sb.replace( i, i + 2, "\n" );
-					}
-					else if( nextCh == 'r' ) {
-						sb.replace( i, i + 2, "\r" );
-					}
-					else if( nextCh == 't' ) {
-						sb.replace( i, i + 2, "\t" );
-					}
-					else {
-						sb.replace( i, i + 2, String.valueOf( nextCh ) );
-					}
-					length--;
-				}
-			}
-
-			string = sb.toString();
+		if( firstBackslashIndex == -1 ) {
+			return string;
 		}
 
-		return string;
+		final StringBuilder sb = new StringBuilder( string );
+
+		for( int i = firstBackslashIndex; i < sb.length(); i++ ) {
+			if( sb.charAt( i ) == '\\' && i + 1 < sb.length() ) {
+				char nextChar = sb.charAt( i + 1 );
+
+				switch( nextChar ) {
+					case 'n' -> sb.replace( i, i + 2, "\n" );
+					case 'r' -> sb.replace( i, i + 2, "\r" );
+					case 't' -> sb.replace( i, i + 2, "\t" );
+					case '\\' -> sb.replace( i, i + 2, "\\" );
+					default -> throw new IllegalArgumentException( "Unknown escape character: '%s' (%s) ".formatted( nextChar, Character.getName( nextChar ) ) );
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * @return The given string converted to a number. If the number contains a decimal separator (period), returns a Double, if no decimal separator, returns an Integer.
+	 */
+	static Number numericValueFromString( final String numericString ) {
+
+		if( numericString.contains( "." ) ) {
+			return Double.valueOf( numericString );
+		}
+
+		// CHECKME: Determine the number's size and return a Long if it doesn't fit in an int? Or just always return a Long? // Hugi 2025-03-19
+		return Integer.valueOf( numericString );
 	}
 
 	/**
 	 * @return true if this is a numeric string. Note that a signed number (i.e. prefixed with a plus or a minus) is considered numeric
 	 */
 	static boolean isNumeric( final String string ) {
+
 		int length = string.length();
 
 		if( length == 0 ) {
 			return false;
 		}
 
-		boolean dot = false;
+		boolean dotAlreadySpotted = false;
+
 		int i = 0;
-		char character = string.charAt( 0 );
+		char character = string.charAt( i );
 
 		if( (character == '-') || (character == '+') ) {
 			i = 1;
 		}
 		else if( character == '.' ) {
 			i = 1;
-			dot = true;
+			dotAlreadySpotted = true;
+		}
+
+		// If we've already advanced and the string's length is only 1, the string is merely a period or a sign and not numeric.
+		if( i == 1 && length == 1 ) {
+			return false;
 		}
 
 		while( i < length ) {
 			character = string.charAt( i++ );
 
 			if( character == '.' ) {
-				if( dot ) {
+				if( dotAlreadySpotted ) {
 					return false;
 				}
-				dot = true;
+				dotAlreadySpotted = true;
 			}
 			else if( !(Character.isDigit( character )) ) {
 				return false;
